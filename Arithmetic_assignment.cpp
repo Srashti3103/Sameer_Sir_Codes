3702aba1e71d39157084bfda7dc77a7c6b56b891
srashti patel <srashtipatel31@gmail.com>
2025-10-11 14:01:02 +0530

    arithmetic assignments

---
+++ Arithmetic_assignment.cpp
@@ -0,0 +1,16 @@
+//arthemetic assignment
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	a += 5;//a=a+5
+	printf("%d\n",a);//15
+	a -= 5;
+	printf("%d\n",a);
+	a *= 5;
+	printf("%d\n",a);
+	a  /= 5;
+	printf("%d\n",a);
+	a %= 5;
+	printf("%d\n",a);	
+}
