8cb78174557aae1e5cc0591d1f96d56bcb01b02d
srashti patel <srashtipatel31@gmail.com>
2025-10-11 14:01:44 +0530

    arthmetic operator

---
+++ arthmetic_operator.cpp
@@ -0,0 +1,17 @@
+//arthemetic operators
+#include<stdio.h>
+int main ()
+{
+	int a =10;
+	printf("%d\n",a);
+	int b=20;
+	printf("%d\n",b);
+	printf("%d%d\n",a,b);
+	printf("%d\n%d",a,b);
+	printf("a=%d",a);
+	printf("b=%d",b);
+	int c;
+	c=a+b;
+	printf("sum of a and b = %d\n",c); 
+	}
+	
