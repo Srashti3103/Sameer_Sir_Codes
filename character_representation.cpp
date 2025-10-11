7456891a27a7561a427082e8c9f02fca1fb59b81
srashti patel <srashtipatel31@gmail.com>
2025-10-11 15:28:53 +0530

    character representation

---
+++ character_representation.cpp
@@ -0,0 +1,20 @@
+//ASCII VALUE
+#include<stdio.h>
+int main()
+{
+	char ch = 'A';
+	printf("ch = %c\n",ch);
+	printf("ASCII VALUE = %d \n", ch);
+	
+	int ch1 =  57;
+	printf("ch1 = %c\n",ch1);
+	printf("ASCII VALUE = %d \n", ch);
+	
+	int ch2 = 'B';
+	printf("ch2 = %c\n",ch2);
+	printf("ASCII VALUE = %d \n", ch2);
+	
+	char ch3 = 67;
+	printf("ch3 = %c\n",ch3);
+	printf("ASCII VALUE = %d \n", ch3);
+}
