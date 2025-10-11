9aba1975dc757762995d84d09ddf4d61ce06dc38
srashti patel <srashtipatel31@gmail.com>
2025-10-11 11:53:34 +0530

    odd even

---
+++ odd_even.cpp
@@ -0,0 +1,17 @@
+//print odd and even number 
+#include<stdio.h>
+int main ()
+{
+	int i = 1;
+	while(i<=30)
+	{
+		printf("%d\n",i);
+		i=i+2;//i++
+	}
+	i = 0;
+	while(i<=30)
+	{
+		printf("%d\n",i);
+		i=i+2;//i++
+	}
+}
