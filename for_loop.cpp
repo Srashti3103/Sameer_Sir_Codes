05c4556964d2ef19e4d110dae65227f45ee3e3d9
srashti patel <srashtipatel31@gmail.com>
2025-10-13 11:11:14 +0530

    for loop

---
+++ for_loop.cpp
@@ -0,0 +1,13 @@
+//conditional statement (for loop)
+#include<stdio.h>
+int main()
+{
+	int i,n ;
+	printf("ENTER n \n");
+	//input the value 
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)//looping
+	{
+		printf("%d\n",i);
+	}
+}
