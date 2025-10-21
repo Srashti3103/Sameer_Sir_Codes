91a93bd5431fe5982b6b7e0ca44a9b9c91190afb
Srashti3103 <srashtipatel31@gmail.com>
2025-10-22 00:31:27 +0530

    reverse of loop

---
+++ PRACTICE/reverse_loop.cpp
@@ -0,0 +1,13 @@
+//REVERSE LOOP
+#include<stdio.h>
+int main()
+{
+	int i,n ;
+	printf("ENTER n \n");
+	//input value of n
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)//looping (reverse loop)
+	{
+		printf("%d\n",i);
+	}
+}
