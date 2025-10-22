3d4e8e3ead5ad5fa3ac40bec80a06751aaafedd5
Srashti3103 <srashtipatel31@gmail.com>
2025-10-23 00:40:34 +0530

    sum of series without using pow

---
+++ PRACTICE/sum_series_without_using_pow.cpp
@@ -0,0 +1,14 @@
+//find_sum_series_without_using_pow()_function
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, num , size ,sum=1,pow=1;
+	printf("Enter num and size :");
+	scanf("%d%d",&num,&size);
+	for(i=1;i<=size;i++){
+		pow=pow*num;
+		sum=sum+pow;
+	}
+	printf("ANS = %d\n",sum);
+}
