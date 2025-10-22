6bc460c82af377e1925d7fbc1d912b8073eef750
Srashti3103 <srashtipatel31@gmail.com>
2025-10-23 00:34:02 +0530

    sum of series

---
+++ PRACTICE/sum_of_series.cpp
@@ -0,0 +1,13 @@
+//sum_of_series
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i, num , size ,sum=1;
+	printf("Enter num and size :");
+	scanf("%d%d",&num,&size);
+	for(i=1;i<=size;i++){
+		sum=sum+pow(num,i);
+	}
+	printf("ANS = %d\n",sum);
+}
