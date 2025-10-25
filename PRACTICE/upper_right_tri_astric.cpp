ef93333c06c11cb8b6a034e8ed9bbf0adac3befa
Srashti3103 <srashtipatel31@gmail.com>
2025-10-25 23:00:57 +0530

    upper right triangle

---
+++ PRACTICE/upper_right_tri_astric.cpp
@@ -0,0 +1,16 @@
+//print pattern (right triangle)
+#include<stdio.h>
+int main ()
+{
+	int n ;
+	printf("Enter the no. of rows \n");
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++)//outer loop for row
+	{
+		for(int j=1; j<=i; j++)//inner loop for coloumn
+		{
+		printf("*");	
+		}
+		printf("\n");
+	}
+}
