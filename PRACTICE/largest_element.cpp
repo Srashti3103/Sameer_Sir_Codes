1d24ca9f513150c989e6c0b7fc8902880bd16d52
Srashti3103 <srashtipatel31@gmail.com>
2025-11-08 14:42:32 +0530

    largest element

---
+++ PRACTICE/largest_element.cpp
@@ -0,0 +1,23 @@
+//print biggest number
+#include<stdio.h>
+int main()
+{
+	int n ;
+	printf("Enter the size of array :");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements of array :");
+	for(int i=0; i<n; i++)
+	{
+		scanf("%d",&arr[i]);
+    }
+    int max = arr[0];
+    for(int i=0; i<n; i++)
+    {
+    	if(max<arr[i])
+    	{
+    		max = arr[i];
+		}
+	}
+	printf("Biggest element of array : %d\n ", max);
+}
