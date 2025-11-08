e6319b1836495368b6d137a71efe68eb979f1e0c
Srashti3103 <srashtipatel31@gmail.com>
2025-11-08 14:43:31 +0530

    diagonal matrix

---
+++ PRACTICE/diagona_matrix.cpp
@@ -0,0 +1,22 @@
+//print diagonal of a matrix 
+#include<stdio.h>
+int main()
+{
+	int a[3][3]={{10,20,30},{40,50,60},{70,80,90}};
+	int m=3, n=3; 
+	for(int i=0 ; i<m ; i++)//row
+	{
+		for(int j=0 ; j<n ; j++)//coloumn
+		{
+			if(i==j)//both index number are same ---->>>> diagonal of a matrix  
+			{	
+			printf("%d",a[i][j]);//print diagonal matrix 
+		    }
+		    else
+		    {
+		    	printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
