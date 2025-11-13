f7cc0424305141f194704b2bc04b3348eb6840e2
Srashti3103 <srashtipatel31@gmail.com>
2025-11-13 21:14:24 +0530

    use of header

---
+++ HEADERS/use_of_multiplication_header.cpp
@@ -0,0 +1,10 @@
+//use of multiplication.h header 
+#include"multiplication.h"
+int main(){
+	int n;
+	float x,t;
+	printf("ENTER x and n \n");
+	scanf("%f%d",&x,&n);
+	t=sinx(x,n);
+	printf("ans=%f\n",t);
+}
