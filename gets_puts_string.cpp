9070fd90037855307c2aac349fbc11870613c4c7
Srashti3103 <srashtipatel31@gmail.com>
2025-11-15 21:17:22 +0530

    gets puts string

---
+++ gets_puts_string.cpp
@@ -0,0 +1,9 @@
+//input special character
+#include<stdio.h>
+int main()
+{
+	char a[40];
+	printf(" ENTER NAME \n");
+	gets( a );//input string (special character)
+	puts( a );//output string (special character)
+}
