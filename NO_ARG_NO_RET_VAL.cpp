d6b0fe456608a4417a34adb3c96f17256bdf8635
Srashti3103 <srashtipatel31@gmail.com>
2025-11-16 10:14:15 +0530

    No argument nad no return value

---
+++ NO_ARG_NO_RET_VAL.cpp
@@ -0,0 +1,12 @@
+//NO ARGUMENT AND NO RETURN VALUE
+//SUM OF TWO NUMBER  
+#include<stdio.h>
+void sum(){
+	int a,b;
+	printf("ENTER THE VALUE OF a AND b :\n");
+	scanf("%d%d",&a,&b);//input 
+	printf("SUM =%d",a+b);//calculation //print
+}
+int main(){
+	sum();//function calling 
+}
