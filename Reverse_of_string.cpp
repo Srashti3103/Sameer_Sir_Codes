0cde2705c3c135d8d10be039a07e36bde8170dde
Srashti3103 <srashtipatel31@gmail.com>
2025-11-16 10:16:24 +0530

    Reverse of string

---
+++ Reverse_of_string.cpp
@@ -0,0 +1,10 @@
+//Reverse of a string 
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char a[10];
+	printf("Enter Name :\n");
+	scanf("%s",a);
+	strrev(a);
+	printf("REVERSE OF a = %s",a);
+}
