8f316fc7012d62352fb6b5c82c5793ed49833f66
Srashti3103 <srashtipatel31@gmail.com>
2025-11-18 00:18:08 +0530

    External storage class

---
+++ External_sstorage_class.cpp
@@ -0,0 +1,17 @@
+//EXTERNAL(GLOBAL)STORAGE CLASS
+//keyword--->>>extern
+//Scope---->>>Global
+//Memory---->>>RAM
+//Default value--->>>Zero
+//To explain external storage class
+#include<stdio.h>
+void fun();
+int main(){
+	extern int a;
+	printf("%d\n",a);
+	fun();
+}
+int a=3;
+void fun(){
+	printf("%d\n",a);
+}
