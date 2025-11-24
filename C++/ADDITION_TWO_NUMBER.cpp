6a6021e20bc1751cb8ba284895ba1f3db4cd1be3
Srashti3103 <srashtipatel31@gmail.com>
2025-11-25 00:39:48 +0530

    Addition of two number

---
+++ C++/ADDITION_TWO_NUMBER.cpp
@@ -0,0 +1,10 @@
+//Write a program to addition of two number 
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout<<"ENTER THE NUMBER "<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"SUM="<<c<<endl;
+}
