942fac808d83766f7f406fe0a09491c63b8063db
Srashti3103 <srashtipatel31@gmail.com>
2025-11-25 00:41:29 +0530

    SWAP CALL BY REFERNCE

---
+++ C++/SWAP_CALL_NY_REFERENCE.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+void swap(int &p,int &q);
+int main(){
+	int a=5,b=2;
+	swap(a,b);
+	cout<<"a="<<a<<endl;
+	cout<<"b="<<a<<endl;
+}
+void swap(int &p,int &q){
+	int c;
+	c=p;
+	p=q;
+	q=c;
+}
