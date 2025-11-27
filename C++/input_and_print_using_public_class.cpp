826c419d3052d1e089593f9918606eeb45da4af4
Srashti3103 <srashtipatel31@gmail.com>
2025-11-28 01:07:42 +0530

    input and print using public class

---
+++ C++/input_and_print_using_public_class.cpp
@@ -0,0 +1,12 @@
+//INPUT AND PRINT USING PUBLIC CLASS
+#include<iostream>
+using namespace std;
+class test{
+	//private : int a;
+	public: int a;
+};
+int main(){
+	test p;
+	p.a=4;
+	cout<<p.a<<endl;
+}
