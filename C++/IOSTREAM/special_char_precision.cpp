f9d7d5510db13d6bda31a2288ed647270b5c5280
Srashti3103 <srashtipatel31@gmail.com>
2025-12-06 16:59:33 +0530

    special char precision

---
+++ C++/IOSTREAM/special_char_precision.cpp
@@ -0,0 +1,14 @@
+//INPUT AND PRINT SPECIAL CHARACTER
+#include<iostream>
+using namespace std;
+int main(){
+	cout.precision(5);
+	cout<<12.16412<<endl;
+	cout.precision(2);
+	cout<<12.16412<<endl;
+	cout<<precision(3);
+	cout<<12.16412<<endl;
+	cout.width(8);
+	cout.fill('*');
+	cout<<123;
+}
